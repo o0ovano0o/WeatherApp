a995ff5ad75f7b78b1666bf0edf20b38187e6267
dominan <minhanh.th99@gmail.com>
2020-04-08 22:48:33 +0700

    splash

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Text, View } from 'react-native';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import Main from './screen/Untitled';
+import Splash from './screen/Splash';
 function useFonts(fontMap) {
   let [fontsLoaded, setFontsLoaded] = useState(false);
   (async () => {
@@ -17,8 +18,8 @@ function useFonts(fontMap) {
 export default props => {
   let [fontsLoaded] = useFonts({
     'montserrat-700': require('./assets/fonts/montserrat-700.ttf'),
-'montserrat-regular': require('./assets/fonts/montserrat-regular.ttf'),
-'verdana-regular': require('./assets/fonts/verdana-regular.ttf')
+    'montserrat-regular': require('./assets/fonts/montserrat-regular.ttf'),
+    'verdana-regular': require('./assets/fonts/verdana-regular.ttf')
   });
 
  if (!fontsLoaded) {
---
+++ screen/Splash.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TextInput,TouchableOpacity,ScrollView,SafeAreaView,Image,Animated} from 'react-native';
+import Constants from 'expo-constants';
+
+export default class Splash extends React.Component {
+
+  state={
+    logoOpacity: new Animated.Value(0),
+    titlemargintop: new Animated.Value(200)
+  }
+  async componentDidMount(){
+    Animated.sequence([
+        Animated.timing(this.state.logoOpacity,{
+            toValue:1,
+            duration: 2000,
+        }),
+        Animated.timing(this.state.titlemargintop,{
+            toValue:10,
+            duration: 1000,
+        }),
+    ]).start(()=>{
+
+    })
+  }
+  render() {
+
+    return (
+      <View style={styles.container}>
+          <Animated.Image style={{...styles.logo, opacity: this.state.logoOpacity}} source={{uri: 'https://images-eu.ssl-images-amazon.com/images/I/61nuuPxUvaL.png'}} />
+          <Animated.Text style={{...styles.title, marginTop: this.state.titlemargintop }}>Weather</Animated.Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#385999',
+    alignItems:'center',
+  },
+ title:{
+   margin:20,
+   fontWeight:'bold',
+   fontSize: 30,
+   fontFamily:'',
+   color:'white'
+ },
+  logo:{
+    height: 100,
+    width:100,
+    borderRadius:50,
+  },
+});
